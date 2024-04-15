2b8db6d7702128d226db713f0957784675216b3a
Anshuman Sharma <anshuman9998@gmail.com>
2024-04-15 23:55:55 +0530

    server setuped with database

---
+++ src/config/serverConfig.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const env = {
+    PORT : process.env.PORT
+}
+
+export default env
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import env from './config/serverConfig';
 
 
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}));
 
 (async()=>{
-    app.listen(3000,()=>{
+    app.listen(env.PORT,()=>{
         console.log("Server started on port 3000");
     })
 })();
